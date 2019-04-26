74b2cde0b9ea897ae92cba6f15f724fa1666402b
fillpot30 <jacobfillpot@gmail.com>
2019-04-26 08:14:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+function button1() {
+a = prompt("what is Question 1?");
+
+elem = document.getElementById("li1"); // Find an element 
+elem.innerHTML = a;           // Display a in the element
+
+var a; // Declare a
+
+}
+
+function button2() {
+b = prompt("what is Question 2?");
+
+elem = document.getElementById("li2"); // Find an element 
+elem.innerHTML = b;           // Display b in the element
+
+var b; // Declare b
+
+}
+
+function button3() {
+c = prompt("what is Question 3?");
+
+elem = document.getElementById("li3"); // Find an element 
+elem.innerHTML = c;           // Display c in the element
+
+var c; // Declare c
+
+}
+
+function button4() {
+d = prompt("what is Question 4?");
+
+elem = document.getElementById("li4"); // Find an element 
+elem.innerHTML = d;           // Display d in the element
+
+var d; // Declare d
+
+}
+
+
 function printpage() {
+
 window.print();
+
+
+
 }
