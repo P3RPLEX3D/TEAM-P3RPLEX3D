19cf85b6d77a36672125428fe5ad248681319322
fillpot30 <jacobfillpot@gmail.com>
2019-04-26 18:14:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,26 @@ window.print();
 
 
 }
+
+var mode = getStoredValue('myPageMode');
+
+function buttonClick(mode) {
+    mode = mode;
+    storeValue('myPageMode', mode);
+}
+
+function storeValue(key, value) {
+    if (localStorage) {
+        localStorage.setItem(key, value);
+    } else {
+        $.cookies.set(key, value);
+    }
+}
+
+function getStoredValue(key) {
+    if (localStorage) {
+        return localStorage.getItem(key);
+    } else {
+        return $.cookies.get(key);
+    }
+}
