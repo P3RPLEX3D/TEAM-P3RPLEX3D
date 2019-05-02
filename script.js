e4e9e1569f4cc06ff23c0ebefe6b06e8a3da2356
fillpot30 <jacobfillpot@gmail.com>
2019-05-02 10:01:35 -0500

    other dice ..still need d10

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function d20() {
 Math.floor(Math.random() * 20 + 1);
 	
 }
+
+function d12() {
+	document.getElementById("d20").innerHTML =
+Math.floor(Math.random() * 12 + 1);
+	
+}
+function d8() {
+	document.getElementById("d20").innerHTML =
+Math.floor(Math.random() * 8 + 1);
+	
+}
+function d6() {
+	document.getElementById("d20").innerHTML =
+Math.floor(Math.random() * 6 + 1);
+	
+}
+function d4() {
+	document.getElementById("d20").innerHTML =
+Math.floor(Math.random() * 4 + 1);
+	
+}
