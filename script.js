54d5c03d7ff45693a41481af04950c6c5af23d23
fillpot30 <jacobfillpot@gmail.com>
2019-05-02 13:29:56 -0500

    construction

--- script.js
+++ script.js
@@ -35,3 +35,17 @@ function d4() {
 Math.floor(Math.random() * 4 + 1);
 	
 }
+
+ function play() {
+        var audio = document.getElementById('audio');
+        if (audio.paused) {
+            audio.play();
+            $('#play').removeClass('glyphicon-play-circle')
+            $('#play').addClass('glyphicon-pause')
+        }else{
+            audio.pause();
+            audio.currentTime = 0
+            $('#play').addClass('glyphicon-play-circle')
+            $('#play').removeClass('glyphicon-pause')
+        }
+    }
