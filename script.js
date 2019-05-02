22527072fa307e9714f7ffd60aeb0e50cd777bfc
fillpot30 <jacobfillpot@gmail.com>
2019-05-02 04:10:45 -0500

    open NAV

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // JavaScript Document
 function myFunction(x) {
   x.classList.toggle("change");
+	
+	
+function openNav() {
+  document.getElementById("myNav").style.width = "25%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 }
