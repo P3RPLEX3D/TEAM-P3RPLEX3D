6d45ce9fd6098b8907293834c709792dfdac84ab
fillpot30 <jacobfillpot@gmail.com>
2019-05-02 07:41:03 -0500

    go

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+
+function d20() {
+	document.getElementById("demo").innerHTML =
+Math.floor(Math.random() * 20 + 1);
+	
+}
